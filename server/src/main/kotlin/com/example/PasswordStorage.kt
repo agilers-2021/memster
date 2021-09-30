9234bde978019effe7c21b6355ecb93f4790dde5
Maria Filipanova <myannyax@gmail.com>
2021-09-30 10:59:05 +0300

    move PasswordErrorDescription

--- server/src/main/kotlin/com/example/PasswordStorage.kt
+++ server/src/main/kotlin/com/example/PasswordStorage.kt
@@ -1,7 +1,12 @@
 package com.example
 
-import com.example.internal.dummyRealization.PasswordErrorDescription
 import com.example.models.Credentials
+
+enum class PasswordErrorDescription {
+  NO_SUCH_USER,
+  INCORRECT_PASSWORD,
+  SUCCESS
+}
 
 interface PasswordStorage {
   fun storeCredentials(credentials: Credentials)
--- server/src/main/kotlin/com/example/internal/dummyRealization/DummyPasswordStorage.kt
+++ server/src/main/kotlin/com/example/internal/dummyRealization/DummyPasswordStorage.kt
@@ -3,12 +3,6 @@ package com.example.internal.dummyRealization
 import com.example.PasswordStorage
 import com.example.models.Credentials
 
-enum class PasswordErrorDescription {
-  NO_SUCH_USER,
-  INCORRECT_PASSWORD,
-  SUCCESS
-}
-
 
 class DummyPasswordStorage : PasswordStorage {
   override fun storeCredentials(credentials: Credentials) {
