0beb75b0bdc9e2526ac26abcd43a07fed6a28404
egdegd <egd3700@mail.ru>
2021-11-24 21:51:01 +0300

    add random photo uploads to your profile

--- user_generation.py
+++ user_generation.py
@@ -2,10 +2,12 @@ import requests
 
 import grequests
 import time
+import base64
 
 
 url = 'http://0.0.0.0:8080/api/'
-picture_url = 'https://picsum.photos/200/300'
+picture_url = 'https://picsum.photos/200'
+
 async_list = []
 
 
@@ -19,8 +21,9 @@ grequests.map(async_list)
 for i in range(10*3):
 	x = requests.post(url + 'authenticate', json={'username': f'Petya{i}', 'password': '123456'})
 	token = x.json()['token']
-	# print(token)
+
 	picture_req = requests.get(picture_url)
-	picture = picture_req.text
-	y = requests.post(url + 'settings', headers={"Authorization": "Bearer " + token}, json={'set_photo': picture_req.text, 'anecdote': 'мой новый анедот'})
-	# print(y)
+	picture = base64.b64encode(picture_req.content)
+
+
+	y = requests.post(url + 'settings', headers={"Authorization": "Bearer " + token}, json={'set_photo': picture, 'anecdote': 'мой новый анедот'})
