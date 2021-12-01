51dcd730b32111c87ffcc356e0e5fc379a396c9d
egdegd <egd3700@mail.ru>
2021-12-01 13:40:26 +0300

    some refactoring

--- user_generation.py
+++ user_generation.py
@@ -1,29 +1,45 @@
 import requests
-
 import grequests
 import time
 import base64
+import names
 
 
-url = 'http://0.0.0.0:8080/api/'
-picture_url = 'https://picsum.photos/200'
+URL = 'http://0.0.0.0:8080/api/'
+number_of_accounts = 10
 
-async_list = []
-
-
-for i in range(10*3):
-	action_item = grequests.post(url + 'register', json={'username': f'Petya{i}', 'password': '123456', 'display_name': 'Kirpich'})
-	async_list.append(action_item)
-
-grequests.map(async_list)
-
-
-for i in range(10*3):
-	x = requests.post(url + 'authenticate', json={'username': f'Petya{i}', 'password': '123456'})
-	token = x.json()['token']
-
+def get_random_photo():
+	picture_url = 'https://picsum.photos/200'
 	picture_req = requests.get(picture_url)
 	picture = base64.b64encode(picture_req.content)
+	return picture
 
 
-	y = requests.post(url + 'settings', headers={"Authorization": "Bearer " + token}, json={'set_photo': picture, 'anecdote': 'мой новый анедот'})
+
+def get_random_anecdote():
+	return "Сидит Йети на горе"
+
+
+def create_accounts():
+
+	async_list = []
+
+	for i in range(number_of_accounts):
+		action_item = grequests.post(URL + 'register', json={'username': f'Petya{i}', 'password': '123456', 'display_name': names.get_first_name()})
+		async_list.append(action_item)
+
+	grequests.map(async_list)
+
+
+def add_info():
+	for i in range(number_of_accounts):
+		x = requests.post(URL + 'authenticate', json={'username': f'Petya{i}', 'password': '123456'})
+		token = x.json()['token']
+
+		picture = get_random_photo()
+		anecdote = get_random_anecdote()
+
+		y = requests.post(URL + 'settings', headers={"Authorization": "Bearer " + token}, json={'new_photos': [picture], 'anecdote': anecdote})
+
+create_accounts()
+add_info()
